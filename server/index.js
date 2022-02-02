9cb04e73a3ab9503ed1ae17f92dd33073e63922e
nideshpandey <nidesh@outlook.com>
2022-02-02 19:33:34 +0545

    Added categories

--- server/index.js
+++ server/index.js
@@ -4,6 +4,9 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/post");
+const categoryRoute = require("./routes/category");
+const multer = require("multer");
+
 
 dotenv.config();
 app.use(express.json());
@@ -12,9 +15,25 @@ mongoose.connect(process.env.DATABASE_URL)
     .then(console.log('Database Connected !'))
     .catch((err) => console.log(err));
 
-    
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "images");
+    },
+    filename: (req, file, cb) => {
+        cb(null, "blog.jpeg");
+    },
+});
+
+const upload = multer({ storage: storage });
+app.post("/server/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
+
+
 app.use("/server/auth", authRoute);
 app.use("/server/post", postRoute);
+app.use("/server/category", categoryRoute);
 
 
 
